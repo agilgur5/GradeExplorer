1705ddcccb26f5d6e2ae00dbbda68457de657a1e
Anton Gilgur <agilgur5@gmail.com>
2016-04-15 14:23:18 -0400

    set up flask server

---
+++ app/application.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, jsonify
+import os
+from Model import basic_prediction_model
+
+app = Flask(__name__)
+# not actually a secret since no need for authentication
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+# standard route
+@app.route('/')
+def loadViz():
+  return render_template('/index.html')
+
+# returns data that specifies what to place into the spec editor
+@app.route('/getPrediction', methods=['GET'])
+def getPrediction():
+  predictGrades = basic_prediction_model.predictGrades
+  return jsonify(predictGrades(request.args.getlist('scores'), 'Model/params'))
+
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  debug = bool(os.environ.get('DEBUG', False))
+  app.run(host='0.0.0.0', port=port, debug=True)
--- run_services.sh
+++ run_services.sh
@@ -3,6 +3,10 @@
 # store initial location to later return to
 INITIAL_LOCATION=$(pwd);
 
+echo 'Running back end services...';
+cd /vagrant/app;
+docker-compose up -d;
+
 echo 'Running front end services...';
 cd /vagrant/static;
 docker-compose up -d;
